8bc13215ce859e001ffa6ddbae1596a38fd5a59c
Shailendra Shinde <shailen14599@mastek.com>
2024-08-14 10:28:13 +0530

    Code updated

--- src/UKHO.S100PermitService.Common/Security/S100Manufacturer.cs
+++ src/UKHO.S100PermitService.Common/Security/S100Manufacturer.cs
@@ -20,15 +20,7 @@
         public string Encrypt(string unencrypted)
         {
             S100Crypt c = new(_key);
-            return BitConverter.ToString(c.Encrypt(StringToByteArray(unencrypted))).Replace("-", "");
-        }
-
-        private static byte[] StringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
+            return BitConverter.ToString(c.Encrypt(unencrypted)).Replace("-", "");
         }
     }
 }
