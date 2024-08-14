a249e30094fca6cd3e702d6fd763a55bfd576a72
Rohit Chettri <rohit15478@mastek.com>
2024-08-14 10:10:55 +0530

    Controller added

---
+++ src/UKHO.S100PermitService.API/Controllers/S100PermitServiceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UKHO.S100PermitService.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class S100PermitServiceController : Controller
+    {
+        public S100PermitServiceController()
+        {
+        }
+
+        [HttpGet(Name = "S100PermitGenerate")]
+        public IActionResult GeneratePermit(string upn, string mkey)
+        {
+            //temp code
+            return new JsonResult(upn+mkey);
+        }
+    }
+}
