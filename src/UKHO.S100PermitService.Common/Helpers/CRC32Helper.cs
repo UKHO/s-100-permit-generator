a9b2ba458e5d048a6c67b1b322b18f0727256167
Nadeem Badai <Nadeem14456@Mastek.Com>
2024-08-22 14:37:08 +0530

    add Crc32Helper class

---
+++ src/UKHO.S100PermitService.Common/Helpers/CRC32Helper.cs
@@ -0,0 +1,15 @@
+﻿using ICSharpCode.SharpZipLib.Checksum;
+using System.Text;
+
+namespace UKHO.S100PermitService.Common.Helpers
+{
+    public static class Crc32Helper
+    {
+        public static string Crc32String(string hwIdEncrypted)
+        {
+            var crc = new Crc32();
+            crc.Update(Encoding.UTF8.GetBytes(hwIdEncrypted));
+            return crc.Value.ToString("X8");
+        }
+    }
+}
