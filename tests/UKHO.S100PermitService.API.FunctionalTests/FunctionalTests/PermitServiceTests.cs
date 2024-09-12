9f1a684607d85100d6e4cbfaeabf00b4bc7a9135
Arit Sarkar <arit14975@mastek.com>
2024-09-12 17:34:05 +0530

    Added FT

--- tests/UKHO.S100PermitService.API.FunctionalTests/FunctionalTests/PermitServiceTests.cs
+++ tests/UKHO.S100PermitService.API.FunctionalTests/FunctionalTests/PermitServiceTests.cs
@@ -1,13 +1,42 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using UKHO.S100PermitService.API.FunctionalTests.Configuration;
+using UKHO.S100PermitService.API.FunctionalTests.Helpers;
 
 namespace UKHO.S100PermitService.API.FunctionalTests.FunctionalTests
 {
-    public class PermitServiceTests
+    public class PermitServiceTests : TestBase
     {
-        [Test]
-        public async Task DummyTest()
+        private AuthTokenProvider _tokenProvider;
+        private TokenConfiguration? _tokenConfiguration;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
         {
-            await Task.CompletedTask;
+            _tokenProvider = new AuthTokenProvider();
+            var _serviceProvider = GetServiceProvider();
+            _tokenConfiguration = _serviceProvider?.GetRequiredService<IOptions<TokenConfiguration>>().Value;
+        }
+
+        [Test]
+        public async Task TestToCheckAuthentication()
+        {
+            var token = await _tokenProvider.GetPSToken(_tokenConfiguration!.ClientId!, _tokenConfiguration.ClientSecret!);
+            Console.WriteLine(token);
+        }
+
+        [Test]
+        public async Task TestToCheckNoAuthentication()
+        {
+            var token = await _tokenProvider.GetPSToken(_tokenConfiguration!.ClientIdNoAuth!, _tokenConfiguration.ClientSecretNoAuth!);
+            Console.WriteLine(token);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Cleanup();
         }
     }
 }
