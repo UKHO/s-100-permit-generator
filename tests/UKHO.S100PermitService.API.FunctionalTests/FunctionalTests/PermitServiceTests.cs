506c998c876166519a989e072c17300a9e7089b0
Shivam Verma <shivam15622@mastek.com>
2024-09-16 10:26:57 +0530

    merged 172720 and FT changes

--- tests/UKHO.S100PermitService.API.FunctionalTests/FunctionalTests/PermitServiceTests.cs
+++ tests/UKHO.S100PermitService.API.FunctionalTests/FunctionalTests/PermitServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using NUnit.Framework;
+using System.Net;
 using UKHO.S100PermitService.API.FunctionalTests.Configuration;
 using UKHO.S100PermitService.API.FunctionalTests.Helpers;
+using FluentAssertions;
 
 namespace UKHO.S100PermitService.API.FunctionalTests.FunctionalTests
 {
@@ -10,6 +12,7 @@ namespace UKHO.S100PermitService.API.FunctionalTests.FunctionalTests
     {
         private AuthTokenProvider _tokenProvider;
         private TokenConfiguration? _tokenConfiguration;
+        private PermitServiceApiConfiguration _apiConfiguration;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
@@ -17,20 +20,23 @@ namespace UKHO.S100PermitService.API.FunctionalTests.FunctionalTests
             _tokenProvider = new AuthTokenProvider();
             var _serviceProvider = GetServiceProvider();
             _tokenConfiguration = _serviceProvider?.GetRequiredService<IOptions<TokenConfiguration>>().Value;
+            _apiConfiguration = _serviceProvider?.GetRequiredService<IOptions<PermitServiceApiConfiguration>>().Value;
         }
 
         [Test]
-        public async Task TestToCheckAuthentication()
+        public async Task TestToResponseForValidUserWithRole()
         {
             var token = await _tokenProvider.GetPSToken(_tokenConfiguration!.ClientId!, _tokenConfiguration.ClientSecret!);
-            Console.WriteLine(token);
+            var response = await PSEndPointHelper.PermitServiceEndPoint(_apiConfiguration!.BaseUrl, token, "2");
+            response.StatusCode.Should().Be((HttpStatusCode)200);
         }
 
         [Test]
-        public async Task TestToCheckNoAuthentication()
+        public async Task TestToResponseForValidUserWithoutRole()
         {
             var token = await _tokenProvider.GetPSToken(_tokenConfiguration!.ClientIdNoAuth!, _tokenConfiguration.ClientSecretNoAuth!);
-            Console.WriteLine(token);
+            var response = await PSEndPointHelper.PermitServiceEndPoint(_apiConfiguration!.BaseUrl, token, "1");
+            response.StatusCode.Should().Be((HttpStatusCode)403);
         }
 
         [OneTimeTearDown]
---
+++ tests/UKHO.S100PermitService.API.FunctionalTests/Helpers/PSEndPointHelper.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKHO.S100PermitService.API.FunctionalTests.Helpers
+{
+    public static class PSEndPointHelper
+    {
+        static readonly HttpClient _httpClient = new();
+        private static string? uri;
+
+        public static async Task <HttpResponseMessage> PermitServiceEndPoint(string? baseUrl, string? accessToken, string licenceId)
+        {
+            uri = $"{baseUrl}/permits/{licenceId}";
+            using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri) ;
+            if(!string.IsNullOrEmpty(accessToken))
+            {
+                httpRequestMessage.Headers.Add("Authorization", "Bearer " + accessToken);
+            }
+            return await _httpClient.SendAsync(httpRequestMessage, CancellationToken.None);
+        }
+
+    }
+}
